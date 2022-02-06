cd7299db33a5a71c9d05b0cd1cebca564173f3cc
tarun-chennur <chennur.tarun@gmail.com>
2022-02-06 22:22:51 +0530

    Added download feature, linked front end with Script.py

--- Script.py
+++ Script.py
@@ -2,9 +2,16 @@ import pandas
 import json
 import csv
 import functions
+import glob
+import os
 
-#open MOCK_DATA.csv file read it and store it in a variable
-df = pandas.read_csv('MOCK_DATA.csv')
+csv_file=glob.glob(os.getcwd()+"/uploads/*.csv")
+j_file=glob.glob(os.getcwd()+"/uploads/*.json")
+ #open MOCK_DATA.csv file read it and store it in a variable
+
+df = pandas.read_csv(csv_file[0])
+
+
 
 #create a dictionary for censored values
 censored_values = {}
@@ -33,21 +40,32 @@ class calls:
 def switch(arg, column):
     getattr(calls , switcher.get(arg))(column) #gets the function name from dictionary based on arg and calls it
 
-#create a json reader to read test.json file
-with open('test.json') as json_file:
-    json_data = json.load(json_file)
+
+
+
+
+def main():
+    
+    #create a json reader to read test.json file
+    with open(j_file[0]) as json_file:
+        json_data = json.load(json_file)
    
-#code block to check if any function applies to column
-for column in df.columns:
-    if column in json_data.keys():
-        switch(json_data.get(column),column)
-    else:
-        censored_values[column] = df[column].tolist()
+    #code block to check if any function applies to column
+    for column in df.columns:
+        if column in json_data.keys():
+            switch(json_data.get(column),column)
+        else:
+            censored_values[column] = df[column].tolist()
+    #write the censored values to a csv file
+    file = open('censored_values.csv', 'w')
+    writefile = csv.writer(file)
+    df_censored = pandas.DataFrame.from_dict(censored_values)
+    df_censored.to_csv('censored_values.csv')
+    
+    file.close()
+    return 1
 
-#write the censored values to a csv file
-file = open('censored_values.csv', 'w')
-writefile = csv.writer(file)
-df_censored = pandas.DataFrame.from_dict(censored_values)
-df_censored.to_csv('censored_values.csv')
-file.close()
 
+
+if __name__=="__main__":
+    main() 
--- render.py
+++ render.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, flash, request, redirect
+from flask import Flask,send_file, render_template, flash, request, redirect
 from werkzeug.utils import secure_filename
 import os
+
 #import magic
  
 app = Flask(__name__)
@@ -21,6 +22,7 @@ def upload_form():
  return render_template('bdaproject-frontend.html')
  
 @app.route('/', methods=['POST'])
+
 def upload_file():
  if request.method == 'POST':
         # check if the post request has the files part
@@ -28,12 +30,48 @@ def upload_file():
    flash('No file part')
    return redirect(request.url)
   files = request.files.getlist('files[]')
+  count =0
   for file in files:
    if file and allowed_file(file.filename):
     filename = secure_filename(file.filename)
     file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-  flash('File(s) successfully uploaded')
+    count+=1
+    flash('File(s) successfully uploaded')
+  if(count==2):  
+    import Script
+    val=Script.main()
+    if(val==1):
+      delete()
+  else: 
+    flash('Incorrect number of files inputted') 
+    
+  #return send_from_directory(directory=filepath, filename="filename")
   return redirect('/')
+
+@app.route('/download')
+def download_file():
+	#path = "html2pdf.pdf"
+	#path = "info.xlsx"
+	path = os.getcwd()+"/censored_values.csv"
+	#path = "sample.txt"
+	return send_file(path, as_attachment=True)
+
+def delete():
+  
+
+  import shutil
+  uploads_dir=os.getcwd()+"/uploads/"
+  
+  for filename in os.listdir(uploads_dir):
+      file_path = os.path.join(uploads_dir, filename)
+      try:
+          if os.path.isfile(file_path) or os.path.islink(file_path):
+              os.unlink(file_path)
+          elif os.path.isdir(file_path):
+              shutil.rmtree(file_path)
+      except Exception as e:
+          print('Failed to delete %s. Reason: %s' % (file_path, e))
+  
    
 if __name__ == '__main__':
  app.run(debug=False)
